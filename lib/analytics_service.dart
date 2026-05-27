a45d0aa65d398fdfd685a88816770d1281997b22
2019chenqq <hccheng091@gmail.com>
2026-05-27 11:42:58 +0800

    [fix]修正畫面追蹤

--- lib/analytics_service.dart
+++ lib/analytics_service.dart
@@ -1,21 +1,28 @@
 import 'package:firebase_analytics/firebase_analytics.dart';
 
 class AnalyticsService {
-  static final FirebaseAnalytics analytics =
-      FirebaseAnalytics.instance;
+  static final FirebaseAnalytics analytics = FirebaseAnalytics.instance;
 
-  static Future<void> logPage(String name) async {
-    await analytics.logScreenView(
-      screenName: name,
+  static Future<void> logScreenView({
+    required String screenName,
+  }) async {
+    await FirebaseAnalytics.instance.logScreenView(
+      screenName: screenName,
+      screenClass: screenName,
     );
   }
+
+  static Future<void> logPage(String name) async {
+    await logScreenView(screenName: name);
+  }
+
   static Future<void> setUserProperty({
-  required String name,
-  required String? value,
-}) async {
-  await FirebaseAnalytics.instance.setUserProperty(
-    name: name,
-    value: value,
-  );
-}
+    required String name,
+    required String? value,
+  }) async {
+    await FirebaseAnalytics.instance.setUserProperty(
+      name: name,
+      value: value,
+    );
+  }
 }
